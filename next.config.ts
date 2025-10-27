96264f9c120f33cee3ea91818c04f5ea9c4c825c
Rhishav Lamichhane <110982820+SelAshyn@users.noreply.github.com>
2025-10-27 22:56:09 +0545

    implement api routes, server actions and caching, and the event details page

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactCompiler: true,
-  experimental: {
-    turbopackFileSystemCacheForDev: true,
-
+  cacheComponents: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+    ]
   },
   async rewrites() {
     return [
